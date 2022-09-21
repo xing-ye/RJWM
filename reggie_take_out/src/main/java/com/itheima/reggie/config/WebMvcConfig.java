c5fd43f23623d38b84c6d6761852e9eef51d3bb0
xing-ye <zhy9306@qq.com>
2022-09-21 16:24:19 +0800

    P6

---
+++ reggie_take_out/src/main/java/com/itheima/reggie/config/WebMvcConfig.java
@@ -0,0 +1,22 @@
+package com.itheima.reggie.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+//说明是配置类
+@Configuration
+@Slf4j
+public class WebMvcConfig extends WebMvcConfigurationSupport  {
+    /*
+    设置静态资源映射
+    classpath是指resources的目录
+     */
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        log.info("开始进行静态资源映射...");
+        registry.addResourceHandler("/backend/**").addResourceLocations("classpath:/backend/");
+        registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");
+    }
+}
