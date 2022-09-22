41850b630815c71276993ed03b88083e9f7d56d5
xing-ye <zhy9306@qq.com>
2022-09-22 15:08:47 +0800

    p7

--- reggie_take_out/src/main/java/com/itheima/reggie/ReggieApplication.java
+++ reggie_take_out/src/main/java/com/itheima/reggie/ReggieApplication.java
@@ -4,8 +4,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@Slf4j
-@SpringBootApplication
+@Slf4j //用于记录log
+@SpringBootApplication //用于启动SpringBoot
 public class ReggieApplication {
     public static void main(String[] args) {
         SpringApplication.run(ReggieApplication.class,args);
